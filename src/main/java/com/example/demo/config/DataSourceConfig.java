353b4dd4d71b0dd3f01b613906886472f03c607e
DASAN <nosignal95@dasansoft.co.kr>
2024-08-07 11:11:44 +0900

    fc

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -3,15 +3,20 @@ package com.example.demo.config;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
+@Configuration
 public class DataSourceConfig {
 
+
     @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
     }
@@ -22,7 +27,7 @@ public class DataSourceConfig {
         sqlSessionFactory.setDataSource(dataSource);
         sqlSessionFactory.setTypeAliasesPackage("com.example.demo");
         sqlSessionFactory.setMapperLocations(
-                new PathMatchingResourcePatternResolver().getResource("mapper/**/*.xml")
+                new PathMatchingResourcePatternResolver().getResource("mapper/BoardMapper.xml")
         );
         return sqlSessionFactory.getObject();
     }
--- src/main/java/com/example/demo/service/BoardVo.java
+++ src/main/java/com/example/demo/service/BoardVo.java
@@ -19,30 +19,4 @@ public class BoardVo {
     String wrt_id;
     Date wrt_dt;
     
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getTtl() {
-		return ttl;
-	}
-	public void setTtl(String ttl) {
-		this.ttl = ttl;
-	}
-	public String getWrt_id() {
-		return wrt_id;
-	}
-	public void setWrt_id(String wrt_id) {
-		this.wrt_id = wrt_id;
-	}
-	public Date getWrt_dt() {
-		return wrt_dt;
-	}
-	public void setWrt_dt(Date wrt_dt) {
-		this.wrt_dt = wrt_dt;
-	}
-    
-    
 }
