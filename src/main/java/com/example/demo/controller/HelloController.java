8376d199b27fad349685db549f5f22cbd4917d62
DASAN <nosignal95@dasansoft.co.kr>
2024-08-26 15:52:32 +0900

    fc

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,30 +1,39 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.CustomerRepository;
 import com.example.demo.service.BoardService;
 import com.example.demo.service.CommentVo;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5500", methods = {RequestMethod.GET, RequestMethod.POST})
 public class HelloController {
 
     @Autowired
     private BoardService boardService;
 
+    private String passphrase = "key...변경";
 
     @GetMapping("/hello")
     public ModelAndView hello(HttpServletRequest request) {
+
+        //     String p1 = request.getParameter("p1");
+        //     p1 = (p1 == null) ? "1" : p1;
+        //     System.out.println("p1 = " + AesCryptUtil.decryptAES(p1, passphrase));
+
         ModelAndView mv = new ModelAndView();
         mv.addObject("hello", boardService.getList());
         mv.setViewName("hello");
         return mv;
+    }
+
+    @GetMapping("/hello1")
+    public Object hello1(HttpServletRequest request) {
+        return boardService.getList();
     }
 
 
